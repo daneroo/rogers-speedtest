983dc0a13f6c83a6a1b5b1854c1e959ae6e737bf
Daniel Lauzon <daniel@piaget.lan>
2020-08-15 19:42:19 -0400

    Ad ping test

--- README.md
+++ README.md
@@ -10,6 +10,13 @@
   - hey/loadimpact
   - nats heartbeat (plus nats-test)
 
+## Ping
+
+See <https://pypi.org/project/pingparsing/#cli-usage>
+
+```bash
+pingparsing -c 5 google.com
+```
 ## Speedtest
 
 
@@ -19,10 +26,12 @@ speedtest-cli --json | jq
 ./loop.sh
 ```
 
-Ouput
+Analyze
 
 ```bash
-cat data/speedtest/*.json|jq -r "[.timestamp,.download,.upload,.error]| @csv"
+cat `find data/speedtest/ -name \*.json` | jq -r "[.timestamp,.download,.upload,.error]| @csv"
+
+cat `find data/ping/ -name \*.json` | jq '.["google.com"].packet_loss_rate'
 ```
 
 ## Setup
@@ -31,10 +40,15 @@ cat data/speedtest/*.json|jq -r "[.timestamp,.download,.upload,.error]| @csv"
 
 ```bash
 sudo apt-get install speedtest-cli jq
+
+sudo apt install python3-pip
+pip3 install pingparsing # as root
 ```
 
 ### On MacOs
 
 ```bash
 brew install speedtest-cli jq
+
+pip3 install pingparsing
 ```
--- loop.sh
+++ loop.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-HH=$(hostname -f)
+FQDN=$(hostname -f)
 
 mkdir -p ./data/speedtest/
+mkdir -p ./data/ping/
 while true; do 
-    STAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-    echo Testing speed for ${HH} at ${STAMP}
-    FILE="./data/speedtest/${HH}-${STAMP}.json"
+    DAY=$(date -u +"%Y-%m-%d")
+    STAMP=$(date -u +"%Y-%m-%dT%H.%M.%SZ")
+
+    echo Testing ping for ${FQDN} at ${STAMP}
+    FILE="./data/ping/${DAY}/${FQDN}-${STAMP}.json"
+    mkdir -p $(dirname $FILE)
+    (pingparsing -c 5 google.com 2>/dev/null || echo "{ \"timestamp\": \"${STAMP}\", \"error\":true }") > ${FILE}
+    cat ${FILE} | jq '.["google.com"].packet_loss_rate'
+
+    echo Testing speed for ${FQDN} at ${STAMP}
+    FILE="./data/speedtest/${DAY}/${FQDN}-${STAMP}.json"
+    mkdir -p $(dirname $FILE)
     (speedtest-cli --json 2>/dev/null || echo "{ \"timestamp\": \"${STAMP}\", \"error\":true }") > ${FILE}
     cat ${FILE} | jq -r "[.timestamp,.download,.upload,.error]| @csv"
+
+
     sleep 260
 done
