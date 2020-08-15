484910b75558b4769d140606c718f2b998470650
Daniel Lauzon <daniel@piaget.lan>
2020-08-15 02:48:23 -0400

    Add error line in json

--- loop.sh
+++ loop.sh
@@ -6,6 +6,6 @@ mkdir -p ./data/speedtest/
 while true; do 
     STAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     echo Testing speed for ${HH} at ${STAMP}
-    speedtest-cli --json | tee ./data/speedtest/${HH}-${STAMP}.json
+    speedtest-cli --json 2>/dev/null || echo "{ \"timestamp\": \"${STAMP}\", \"error\":true }" | tee ./data/speedtest/${HH}-${STAMP}.json
     sleep 30
 done
