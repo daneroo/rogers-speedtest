9bf8bd998ebddea481d61e34cac2bebdf66f3695
Daniel Lauzon <daniel@piaget.lan>
2020-08-15 02:33:34 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+# Service Availability - Speedtest
+
+## Speedtest
+
+- Could put n cron//
+
+```bash
+speedtest-cli --json | jq
+
+./loop.sh
+```
+
+## Setup
+
+### On MacOs
+
+```bash
+brew install speedtest-cli jq
+```
---
+++ loop.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+HH=$(hostname)
+
+mkdir -p ./data/speedtest/
+while true; do 
+    STAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    echo Testing speed for ${HH} at ${STAMP}
+    speedtest-cli --json | tee ./data/speedtest/${HH}-${STAMP}.json
+    sleep 30
+done
