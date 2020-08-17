2034f6f5b952e249e05766d982aca46e4e2608bc
Daniel Lauzon <daniel@piaget.lan>
2020-08-17 01:25:10 -0400

    add jq to once.sh script

--- once.sh
+++ once.sh
@@ -5,7 +5,7 @@ DAY=$(date -u +"%Y-%m-%d")
 STAMP=$(date -u +"%Y-%m-%dT%H.%M.%SZ")
 
 echo Testing ping for ${FQDN} at ${STAMP}
-pingparsing --indent 0 -c 5 -w 5 google.com speedtestslnt.rogers.com
+pingparsing --indent 0 -c 5 -w 5 google.com speedtestslnt.rogers.com | jq
 
 echo Testing speed for ${FQDN} at ${STAMP}
 speedtest-cli --json | jq
